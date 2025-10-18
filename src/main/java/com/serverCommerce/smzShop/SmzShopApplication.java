0425a162a46ff90d18b9662fcdb4e74cb7291f87
iseungmin <sm1111176@gmail.com>
2025-10-18 15:00:44 +0900

    init

---
+++ src/main/java/com/serverCommerce/smzShop/SmzShopApplication.java
@@ -0,0 +1,13 @@
+package com.serverCommerce.smzShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmzShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmzShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serverCommerce/smzShop/SmzShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serverCommerce.smzShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmzShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
